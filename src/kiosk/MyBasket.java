65cf43b452e56ffe6eb4f3bf54c8662eedc09535
BackDevWook <https://github.com/BackDevWook>
2025-03-10 20:08:15 +0900

    장바구니 가격이 잘못 책정되는 버그 수정

--- src/kiosk/MyBasket.java
+++ src/kiosk/MyBasket.java
@@ -9,23 +9,25 @@ class basketForm {
     private String selectMenuName; // 선택된 메뉴의 이름
     private int quantity; // 주문 수량
     private int price; // 가격 ( 1개 가격 * 수량 )
+    private int totalPrice; // 수량 x 가격
 
     // 생성자
     basketForm(String selectMenuName, int quantity, int price) {
         this.selectMenuName = selectMenuName;
         this.quantity = quantity;
         this.price = price;
+        this.totalPrice = price * quantity;
     }
 
     // 주문 수량 조절
     public void controlQuantity(int quantity) {
         this.quantity = quantity;
-        this.price = this.quantity * this.price;
+        this.totalPrice = price * quantity;
     }
 
     @Override
     public String toString() { // 해시태그 출력 방지
-        return selectMenuName + " " + quantity + "ea " + price + "원";
+        return selectMenuName + " " + quantity + "ea " + totalPrice + "원";
     }
 
     // 장바구니 메뉴 별 가격 게터
